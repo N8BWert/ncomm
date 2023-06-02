27a12cbba0f83caf9b481a41bfab45108238ceed
N8BWert <n8.wert.b@gmail.com>
2023-06-02 14:27:44 -0400

    break apart executor trait into single and multi threaded traits

--- src/executor.rs
+++ src/executor.rs
@@ -1,21 +1,14 @@
 pub mod simple_executor;
+pub mod simple_multi_executor;
+pub mod node_wrapper;
 
 use crate::node::Node;
 
 /// An executor should contain a large number of nodes which it will handle the calling
 /// of the update, start and other functions (an executor will take place on a singular thread)
 pub trait Executor<'a> {
-    /// Adds a given node to this executors internal representation of nodes
-    fn add_node(&mut self, new_node: &'a mut dyn Node);
-
     /// Sets the start time for executors allowing for the update loop to begin
     fn start(&mut self);
-
-    /// The main loop for this executor where it will call update on all of its nodes
-    fn update(&mut self);
-
-    /// Run the update loop for a set amount of iterations (usefull for testing)
-    fn update_for(&mut self, iterations: u128);
 
     /// Run the update loop for a set amount of time
     fn update_for_seconds(&mut self, seconds: u128);
@@ -29,4 +22,24 @@ pub trait Executor<'a> {
     /// A useful function to log any important information about this executor (will
     /// be mostly used for errors)
     fn log(&self);
+}
+
+/// Trait to be implemented by all single threaded executors to allow the addition of nodes
+/// in the single threaded context.
+pub trait SingleThreadedExecutor<'a> {
+    /// Adds a given node to the single threaded executor's internal representation of nodes
+    fn add_node(&mut self, new_node: &'a mut dyn Node);
+
+    /// Run the update loop for one iteration
+    fn update(&mut self);
+
+    /// Run the update loop for a set number of iterations
+    fn update_for(&mut self, iterations: u128);
+}
+
+/// Trait to be implemented by all multi threaded executors to allow the addition
+/// of nodes into specific threads of the multi threaded executor.
+pub trait MultiThreadedExecutor<'a> {
+    /// Adds a given node to the specific thread of the multi threaded executor
+    fn add_node_to(&mut self, new_node: &'a mut dyn Node, thread: String);
 }
