606081078adbd1384e53050e15ba93709c1b0f4b
N8BWert <n8.wert.b@gmail.com>
2023-06-02 14:28:13 -0400

    extract node wrapper for use in other executors

---
+++ src/executor/node_wrapper.rs
@@ -0,0 +1,33 @@
+use crate::node::Node;
+
+use std::{cmp::{Ord, Ordering}};
+
+/// Wrapper for a node that gives it a priority based on its update rate
+/// 
+/// Params:
+///     priorty: the priority of the node (i.e. the timestamp of the next update)
+///     node: the node that will be updated after the priority timestamp it reached
+pub(in crate::executor) struct NodeWrapper<'a> {
+    pub priority: u128,
+    pub node: &'a mut dyn Node,
+}
+
+impl Ord for NodeWrapper<'_> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.priority.cmp(&other.priority).reverse()
+    }
+}
+
+impl PartialOrd for NodeWrapper<'_> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for NodeWrapper<'_> {
+    fn eq(&self, other: &Self) -> bool {
+        self.priority == other.priority
+    }
+}
+
+impl Eq for NodeWrapper<'_> {}
