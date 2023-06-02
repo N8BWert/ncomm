4e58482436b692bc3d2726d58096260b3472877e
N8BWert <n8.wert.b@gmail.com>
2023-06-02 16:55:38 -0400

    write preliminary simple multi threaded executor (tests to come)

--- src/executor/simple_multi_executor.rs
+++ src/executor/simple_multi_executor.rs
@@ -1,28 +1,33 @@
-use crate::executor::{Executor, SingleThreadedExecutor, MultiThreadedExecutor, node_wrapper::NodeWrapper, simple_executor::SimpleExecutor};
+use crate::executor::{Executor, SingleThreadedExecutor, MultiThreadedExecutor, simple_executor::SimpleExecutor};
 use crate::node::Node;
 
-use std::{collections::{BinaryHeap, HashMap}, time::{Duration, SystemTime, UNIX_EPOCH}};
+use std::collections::HashMap;
+use std::time::{Duration};
 use std::thread;
+use std::sync::{mpsc, mpsc::{Receiver, Sender}};
 
 pub struct SimpleMultiExecutor<'a> {
     threads: HashMap<String, SimpleExecutor<'a>>,
-    start_time: u128,
-    interrupted: bool
+    interrupt_txs: Vec<Sender<bool>>,
+    interrupt_rx: Receiver<bool>,
+    interrupted: bool,
 }
 
 impl<'a> SimpleMultiExecutor<'a> {
     /// Create a new empty Simple Executor
-    pub fn new() -> Self {
+    pub fn new(interrupt_rx: Receiver<bool>) -> Self {
         Self {
             threads: HashMap::new(),
-            start_time: 0,
+            interrupt_txs: Vec::new(),
+            interrupt_rx,
             interrupted: false
         }
     }
 
     /// Creates a new Simple Executor with the nodes given
-    pub fn new_with(nodes: Vec<(String, &'a mut dyn Node)>) -> Self {
+    pub fn new_with(nodes: Vec<(String, &'a mut dyn Node)>, interrupt_rx: Receiver<bool>) -> Self {
         let mut map: HashMap<String, SimpleExecutor> = HashMap::new();
+        let mut interrupt_txs = Vec::new();
 
         for (thread, node) in nodes {
             match map.get_mut(&thread) {
@@ -30,7 +35,9 @@ impl<'a> SimpleMultiExecutor<'a> {
                     executor.add_node(node);
                 },
                 None => {
-                    let mut executor = SimpleExecutor::new();
+                    let (interrupt_tx, interrupt_rx) = mpsc::channel();
+                    interrupt_txs.push(interrupt_tx);
+                    let mut executor = SimpleExecutor::new(interrupt_rx);
                     executor.add_node(node);
                     map.insert(thread, executor);
                 }
@@ -39,7 +46,8 @@ impl<'a> SimpleMultiExecutor<'a> {
 
         Self {
             threads: map,
-            start_time: 0,
+            interrupt_txs,
+            interrupt_rx,
             interrupted: false
         }
     }
@@ -52,7 +60,9 @@ impl<'a> MultiThreadedExecutor<'a> for SimpleMultiExecutor<'a> {
                 executor.add_node(new_node);
             },
             None => {
-                let mut executor = SimpleExecutor::new();
+                let (interrupt_tx, interrupt_rx) = mpsc::channel();
+                self.interrupt_txs.push(interrupt_tx);
+                let mut executor = SimpleExecutor::new(interrupt_rx);
                 executor.add_node(new_node);
                 self.threads.insert(thread, executor);
             }
@@ -68,18 +78,103 @@ impl<'a> Executor<'a> for SimpleMultiExecutor<'a> {
     }
 
     fn update_for_seconds(&mut self, seconds: u128) {
-        
+        let (thread_names, executors) = thread::scope(|scope| {
+            let mut thread_names = Vec::with_capacity(self.threads.len());
+            let mut handles = Vec::with_capacity(self.threads.len());
+            let mut executors = Vec::with_capacity(self.threads.len());
+
+            for (thread_name, mut executor) in self.threads.drain() {
+                thread_names.push(thread_name);
+                handles.push(scope.spawn(|| {
+                    executor.start();
+                    executor.update_loop();
+                    return executor;
+                }));
+            }
+
+            // TODO: In the future the main thread can probably do something useful
+            // other than just sleeping
+            thread::sleep(Duration::from_secs(seconds as u64));
+
+            for interrupt_tx in &self.interrupt_txs {
+                interrupt_tx.send(true).unwrap();
+            }
+
+            self.log("Interrupt Sent Waiting For Wind Down");
+
+            for handle in handles.drain(..) {
+                executors.push(handle.join().unwrap());
+            }
+
+            self.log("Wind Down Complete");
+
+            return (thread_names, executors);
+        });
+
+        let mut map = HashMap::new();
+        for (thread_name, executor) in thread_names.iter().zip(executors) {
+            map.insert(thread_name.clone(), executor);
+        }
+
+        self.threads = map;
     }
 
     fn update_loop(&mut self) {
-        
+        let (thread_names, executors) = thread::scope(|scope| {
+            let mut thread_names = Vec::with_capacity(self.threads.len());
+            let mut handles = Vec::with_capacity(self.threads.len());
+            let mut executors = Vec::with_capacity(self.threads.len());
+
+            for (thread_name, mut executor) in self.threads.drain() {
+                thread_names.push(thread_name);
+                handles.push(scope.spawn(|| {
+                    executor.start();
+                    executor.update_loop();
+                    return executor;
+                }));
+            }
+
+            while !self.check_interrupt() {
+                // TODO: In the future the main thread can probably do something useful
+                // other than just sleeping
+                thread::sleep(Duration::from_secs(2));
+            }
+
+            for interrupt_tx in &self.interrupt_txs {
+                interrupt_tx.send(true).unwrap();
+            }
+
+            self.log("Interrupt Sent Waiting For Wind Down");
+
+            for handle in handles.drain(..) {
+                executors.push(handle.join().unwrap());
+            }
+
+            self.log("Wind Down Complete");
+
+            return (thread_names, executors);
+        });
+
+        let mut map = HashMap::new();
+        for (thread_name, executor) in thread_names.iter().zip(executors) {
+            map.insert(thread_name.clone(), executor);
+        }
+
+        self.threads = map;
     }
 
-    fn interrupt(&mut self) {
-        
+    fn check_interrupt(&mut self) -> bool {
+        let iter = self.interrupt_rx.try_iter();
+        if let Some(interrupt) = iter.last() {
+            if interrupt != self.interrupted {
+                self.interrupted = interrupt;
+            }
+        }
+        return self.interrupted;
     }
 
-    fn log(&self) {
-        
+    fn log(&self, message: &str) {
+        // TODO: In the future we'll probably do something more than just print to the console
+        println!("{}", message);
     }
 }
