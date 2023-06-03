ed748f7b358403db24cd9bcf3e729edac768b0b9
N8BWert <n8.wert.b@gmail.com>
2023-06-03 10:02:02 -0400

    update minimal_service_client to use ncomm 0.1.1

--- examples/minimal_service_client/src/main.rs
+++ examples/minimal_service_client/src/main.rs
@@ -1,6 +1,8 @@
 extern crate ncomm;
 
-use ncomm::executor::{Executor, simple_executor::SimpleExecutor};
+use std::sync::mpsc;
+
+use ncomm::executor::{Executor, SingleThreadedExecutor, simple_executor::SimpleExecutor};
 
 pub mod minimal_service;
 pub mod minimal_client;
@@ -9,12 +11,14 @@ use minimal_client::MinimalClient;
 use minimal_service::MinimalService;
 
 fn main() {
+    let (_interrupt_tx, interrupt_rx) = mpsc::channel();
+
     let mut minimal_service = MinimalService::new("minimal service");
     let mut minimal_client = MinimalClient::new("minimal client", minimal_service.create_client(String::from("minimal client")));
-    let mut executor = SimpleExecutor::new();
+    let mut executor = SimpleExecutor::new(interrupt_rx);
+
     executor.add_node(&mut minimal_service);
     executor.add_node(&mut minimal_client);
     executor.start();
     executor.update_for_seconds(1);
-    executor.interrupt();
 }
