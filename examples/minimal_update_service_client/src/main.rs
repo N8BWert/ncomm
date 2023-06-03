11ea71e578a3b0cd996d0b43c736e1c86d87654b
N8BWert <n8.wert.b@gmail.com>
2023-06-03 10:02:19 -0400

    update minimal_update_service_client to use ncomm 0.1.1

--- examples/minimal_update_service_client/src/main.rs
+++ examples/minimal_update_service_client/src/main.rs
@@ -1,6 +1,8 @@
 extern crate ncomm;
 
-use ncomm::executor::{Executor, simple_executor::SimpleExecutor};
+use std::sync::mpsc;
+
+use ncomm::executor::{Executor, SingleThreadedExecutor, simple_executor::SimpleExecutor};
 
 pub mod fibonacci_update_server;
 pub mod fibonacci_update_client;
@@ -9,12 +11,14 @@ use fibonacci_update_server::FibonacciUpdateServer;
 use fibonacci_update_client::FibonacciUpdateClient;
 
 fn main() {
+    let (_interrupt_tx, interrupt_rx) = mpsc::channel();
+
     let mut server = FibonacciUpdateServer::new("fibonacci update server");
     let mut client = FibonacciUpdateClient::new("fibonacci update client", server.create_client(String::from("fibonacci update client")));
-    let mut executor = SimpleExecutor::new();
+    let mut executor = SimpleExecutor::new(interrupt_rx);
+
     executor.add_node(&mut server);
     executor.add_node(&mut client);
     executor.start();
     executor.update_for_seconds(1);
-    executor.interrupt();
 }
